0ca719a2ad5466c6fcd636a29a1f90e2a065326b
Sourish Dutta <sourish.d@gmail.com>
2021-07-25 12:50:11 -0500

    BoardMarkings added

--- src/board/board.js
+++ src/board/board.js
@@ -2,17 +2,8 @@ import React from "react"
 import Square from "../Square/Square"
 import Tile from "../Tile/Tile"
 import styles from "./Board.module.css"
+import { TWs, DWs, TLs, S } from "./BoardMarkings"
 
-const loc = (down,across) => { return 15*(down-1)+across-1}
-
-const TWs = [loc(1,1), loc(1,8), loc(1,15), loc(8,1), loc(8,8), loc(8,15), loc(15,1), loc(15,8), loc(15,15)]
-const DWs = [loc(2,2), loc(2,14), 
-            loc(3,3), loc(3,13), 
-            loc(4,4), loc(4,12), 
-            loc(5,5), loc(5,11), 
-            loc(11,5), loc(11,11)]
-const TLs = [20, 24]
-const S = 112
 
 const renderSquare = (i, piece = null) => {
     let whichBgd = ""
