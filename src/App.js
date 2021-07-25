967657c12fe83665cc6953226f65f1d384372150
Sourish Dutta <sourish.d@gmail.com>
2021-07-25 03:47:15 -0500

    tile list moved to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Board from './Board/Board';
 
-
+const TILE_LIST = new Map()
+TILE_LIST.set(5, ["P", 2])
+TILE_LIST.set(36, ["X", 10])
+TILE_LIST.set(200, ["E", 1])
+TILE_LIST.set(115, ["O", 1])
 
 
 function App() {
   return (
     <div>
-      <Board/>
+      <Board tiles={TILE_LIST}/>
     </div>
   );
 }
--- src/board/board.js
+++ src/board/board.js
@@ -30,16 +30,13 @@ const renderSquare = (i, piece=null) => {
     )
 
 }
-const TILE_LIST = new Map()
-TILE_LIST.set(5, ["P", 2])
-TILE_LIST.set(36, ["X", 10])
-TILE_LIST.set(200, ["E", 1])
-const Board = () => {
+
+const Board = ( {tiles} ) => {
 
     const squares = []
     for (let i =0;i<225;i++ ){
-        if (TILE_LIST.has(i)){
-            let piece = <Tile letter={TILE_LIST.get(i)[0]} points={TILE_LIST.get(i)[1]}/>
+        if (tiles.has(i)){
+            let piece = <Tile letter={tiles.get(i)[0]} points={tiles.get(i)[1]}/>
             squares.push(renderSquare(i, piece))
         }
         else {
