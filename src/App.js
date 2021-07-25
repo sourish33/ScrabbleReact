6541431a28f12bd8852ab2082d17ec431b1a2601
Sourish Dutta <sourish.d@gmail.com>
2021-07-24 19:41:45 -0500

    Tile added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Tile from './Tile/Tile';
+
+
+function App() {
+  return (
+    <div>
+      <Tile letter="T" points="1"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Tile/Tile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Tile.module.css'
+
+const Tile = (props) => {
+    return (
+        <div className={styles.tile}>
+            <div className={styles.letter}>{props.letter}</div>
+            <div className={styles.points}>{props.points}</div>
+        </div>
+
+    )
+
+}
+
+export default Tile
