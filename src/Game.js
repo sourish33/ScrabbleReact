416ae0622711cb8db8976aad414795c18d515c45
Sourish Dutta <sourish.d@gmail.com>
2021-07-27 18:18:04 -0500

    arrayToMap brought back and dummyData file created

--- src/Game.js
+++ src/Game.js
@@ -1,24 +1,9 @@
 import React, { useState } from "react"
 import Board from "./Board/Board"
-import { getUniqueInts } from "./Utils/helpers"
-
-let arr=getUniqueInts(6)
-const TILE_LIST_ARR = [
-// {pos: 15*7+7, letter: "A", points: 1},
-{pos: arr[5], letter: "A", points: 1},
-{pos: arr[0], letter: "B", points: 2},
-{pos: arr[1], letter: "P", points: 1},
-{pos: arr[2], letter: "S", points: 1},
-{pos: arr[3], letter: "E", points: 1},
-{pos: arr[4], letter: "T", points: 1},
-]
-// const TILE_LIST_ARR = []
-
-
+import { TILE_LIST_MAP } from "./Utils/DummyData.js"
 
 const Game = () => {
-    const [tiles, setTiles] = useState(TILE_LIST_ARR)
-    
+    const [tiles, setTiles] = useState(TILE_LIST_MAP)
 
     return (
         <div>
