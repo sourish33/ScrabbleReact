3b9b44f0d24d34c66e8623410745f46ad57857ad
Sourish Dutta <sourish.d@gmail.com>
2021-07-29 15:17:23 -0500

    doesn't do anything if bringing back to the same location

--- src/Game.js
+++ src/Game.js
@@ -1,46 +1,62 @@
 import React, { useState } from "react"
-import Board from './Board/Board'
-import { TILE_LIST_ARR} from './Utils/DummyData'
-
-
+import Board from "./Board/Board"
+import { TILE_LIST_ARR } from "./Utils/DummyData"
 
 const Game = () => {
     const [tiles, setTiles] = useState(TILE_LIST_ARR)
 
-    const move = (origin, destination) =>{
-      if (tiles.find(el=>{ return el.pos===destination})) {
-        console.log("occupied spot")
-        return
-      }
+    const move = (origin, destination) => {
+        if (origin === destination) {
+            console.log("Back to the same location")
+            return
+        }
+        if (
+            tiles.find((el) => {
+                return el.pos === destination
+            })
+        ) {
+            console.log("occupied spot")
+            return
+        }
 
-
-      let whatsHere=tiles.find(el=>{ return el.pos===origin})
-      setTiles((x)=>{
-        x = x.filter((el)=>{return el.pos!==origin})
-        return [...x, {pos: destination, letter: whatsHere.letter, points: whatsHere.points, submitted: whatsHere.submittedr}]
-      })
-
+        let whatsHere = tiles.find((el) => {
+            return el.pos === origin
+        })
+        setTiles((x) => {
+            x = x.filter((el) => {
+                return el.pos !== origin
+            })
+            return [
+                ...x,
+                {
+                    pos: destination,
+                    letter: whatsHere.letter,
+                    points: whatsHere.points,
+                    submitted: whatsHere.submitted,
+                },
+            ]
+        })
     }
     const DragStart = (event) => {
-      let whereArtThou = event.target.parentElement.parentElement.id
-      event.dataTransfer.setData("text/plain", whereArtThou)
-      // console.log(event.target.parentElement.parentElement.id)
-  }
-  
-  const DragOver = (event) => {
-      event.preventDefault()
-  }
-  
-  const Drop = (event) => {
-      event.preventDefault()
-      let incoming = parseInt(event.dataTransfer.getData("text"))
-      let dest = parseInt(event.currentTarget.id)
-      console.log(`${incoming} to ${dest}`)
-      move(incoming, dest)
-  }
+        let whereArtThou = event.target.parentElement.parentElement.id
+        event.dataTransfer.setData("text/plain", whereArtThou)
+        // console.log(event.target.parentElement.parentElement.id)
+    }
+
+    const DragOver = (event) => {
+        event.preventDefault()
+    }
+
+    const Drop = (event) => {
+        event.preventDefault()
+        let incoming = parseInt(event.dataTransfer.getData("text"))
+        let dest = parseInt(event.currentTarget.id)
+        console.log(`${incoming} to ${dest}`)
+        move(incoming, dest)
+    }
 
     // const handleClick = (event) =>{
-    
+
     //   let clickedSquareId=parseInt(event.currentTarget.id)
     //   let whatsHere=tiles.find(el=>{ return el.pos===clickedSquareId})
     //   if (!whatsHere) {
@@ -53,11 +69,12 @@ const Game = () => {
 
     return (
         <div>
-            <Board 
-            tiles={tiles} 
-            DragStart={DragStart}
-            DragOver={DragOver}
-            Drop={Drop}/>
+            <Board
+                tiles={tiles}
+                DragStart={DragStart}
+                DragOver={DragOver}
+                Drop={Drop}
+            />
         </div>
     )
 }
