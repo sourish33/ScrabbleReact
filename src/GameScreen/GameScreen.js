8505c0c91d499e469e33ddf31e221304dc84c6d1
Sourish Dutta <sourish.d@gmail.com>
2021-08-02 20:30:53 -0500

    button alignment horizontal on small screen

--- src/GameScreen/GameScreen.js
+++ src/GameScreen/GameScreen.js
@@ -1,6 +1,11 @@
 import React from "react"
 import { Button, Col, Container, Row } from "react-bootstrap"
 import Game from "../Game"
+import styles from './GameScreen.module.css'
+<meta
+  name="viewport"
+  content="minimum-scale=1, initial-scale=1, width=device-width"
+/>
 
 const GameScreen = () => {
     return (
@@ -9,13 +14,13 @@ const GameScreen = () => {
                 <Col sm={12} lg={7}>
                     <Game visibleRack="p"></Game>
                 </Col>
-                <Col sm={12} lg={2} className="d-flex flex-lg-column flex-sm-row">
-                    <Button className="btn btn-primary">Shuffle</Button>
-                    <Button className="btn btn-primary mt-2">Recall</Button>
-                    <Button className="btn btn-primary mt-2">Exchange</Button>
-                    <Button className="btn btn-danger mt-2">Pass</Button>
-                    <Button className="btn btn-info mt-2">Dictionary</Button>
-                    <Button className="btn btn-success mt-2">Play</Button>
+                <Col sm={12} lg={2} className={`d-flex flex-lg-column ${styles['horz-btns']}`}>
+                    <Button className="mt-2" variant="primary" disabled={false}>Shuffle</Button>
+                    <Button className="mt-2" variant="primary" disabled={false}>Recall</Button>
+                    <Button className="mt-2" variant="primary" disabled={false}>Exchange</Button>
+                    <Button className="mt-2" variant="warning" disabled={false}>Pass</Button>
+                    <Button className="mt-2" variant="info">Dictionary</Button>
+                    <Button className="mt-2" variant="success" disabled={false}>Play</Button>
                 </Col>
                 <Col sm={12} lg={3}>
                     3 of 3
