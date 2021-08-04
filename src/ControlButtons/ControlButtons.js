7f35a22ead2bff91466494a09ef9b5b375d716bb
Sourish Dutta <sourish.d@gmail.com>
2021-08-04 14:12:20 -0500

    buttons made fixed width on large screen

--- src/ControlButtons/ControlButtons.js
+++ src/ControlButtons/ControlButtons.js
@@ -6,31 +6,31 @@ const ControlButtons = (props) => {
     return (
         <div className={`d-flex flex-lg-column ${styles["horz-btns"]}`}>
             <div class="p-2 mt-0">
-                <Button variant="primary" disabled={props.disabled}>
+                <Button className={styles.stretch} variant="primary" disabled={props.disabled}>
                 <span className={styles.smallscreen}>Shuffle</span>
                 </Button>
             </div>
             <div class="p-2 mt-0">
-                <Button variant="primary" disabled={props.disabled}>
+                <Button className={styles.stretch} variant="primary" disabled={props.disabled}>
                 <span className={styles.smallscreen}>Recall</span>
                 </Button>
             </div>
             <div class="p-2 mt-0">
-                <Button variant="primary" disabled={props.disabled}>
+                <Button className={styles.stretch} variant="primary" disabled={props.disabled}>
                 <span className={styles.smallscreen}>Exchange</span>
                 </Button>
             </div>
             <div class="p-2 mt-0">
-                <Button variant="warning" disabled={props.disabled}>
+                <Button className={styles.stretch} variant="warning" disabled={props.disabled}>
                 <span className={styles.smallscreen}>Pass</span>
                     
                 </Button>
             </div>
             <div class="p-2 mt-0">
-                <Button variant="info"><span className={styles.smallscreen}>Dict</span></Button>
+                <Button className={styles.stretch} variant="info"><span className={styles.smallscreen}>Dict</span></Button>
             </div>
             <div class="p-2 mt-0">
-                <Button variant="success" disabled={props.disabled}>
+                <Button className={styles.stretch} variant="success" disabled={props.disabled}>
                 <span className={styles.smallscreen}>Play</span>
                 </Button>
             </div>
