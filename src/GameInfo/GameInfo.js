1ccf0f4dc9a110eabe24a7dee95b09b4dbc4856e
Sourish Dutta <sourish.d@gmail.com>
2021-08-08 18:27:34 -0500

    minor changes

--- src/GameInfo/GameInfo.js
+++ src/GameInfo/GameInfo.js
@@ -3,7 +3,6 @@ import { Form, Button, Row, Col } from "react-bootstrap"
 import { shuffle } from "../Utils/helpers"
 import styles from "./GameInfo.module.css"
 
-
 let AI_LIST = shuffle([
     "AI Lily",
     "AI Tori",
@@ -32,18 +31,17 @@ const GameInfo = ({ handleSubmit }) => {
         let x = [...players]
         x[index]["level"] = playerLevel
         setPlayers(x)
-
     }
 
     const handleSelectShuffle = (event) => {
-        setShufflePlayers(event.target.value) 
+        setShufflePlayers(event.target.value)
     }
 
-    const handleSelectDictCheck = (event) =>{
+    const handleSelectDictCheck = (event) => {
         setDictCheck(event.target.value)
     }
 
-    const handleSelectGameType = (event) =>{
+    const handleSelectGameType = (event) => {
         setGameType(event.target.value)
     }
 
@@ -81,7 +79,11 @@ const GameInfo = ({ handleSubmit }) => {
             ) : (
                 <div key={ind}>
                     <input value={el.name} type="text" disabled />
-                    <select name="level" onChange={(e)=>handleSelect(e,ind)} value={el.level}>
+                    <select
+                        name="level"
+                        onChange={(e) => handleSelect(e, ind)}
+                        value={el.level}
+                    >
                         <option value="1">Weak</option>
                         <option value="2">Medium</option>
                         <option value="3">Strong</option>
@@ -93,98 +95,99 @@ const GameInfo = ({ handleSubmit }) => {
     })
 
     const ShouldShuffle = () => {
-        return(
+        return (
             <div className={styles.horz}>
                 <div>Shuffle Players? </div>
                 <div className="ml-4">
-                    <select name="shuffle" onChange={handleSelectShuffle} value={shufflePlayers}>
+                    <select
+                        name="shuffle"
+                        onChange={handleSelectShuffle}
+                        value={shufflePlayers}
+                    >
                         <option value="0">No</option>
                         <option value="1">Yes</option>
                     </select>
                 </div>
-                {/* <div className="ml-4" onChange={handleSelectShuffle} value={shufflePlayers}>
-                     <input type="radio" value="0" name="No"/> No
-                    <input type="radio" value="1" name="Yes"/> Yes
-                </div> */}
             </div>
-
         )
     }
 
     const ShouldCheckDict = () => {
-        return(
+        return (
             <div className={styles.horz}>
                 <div>Dictionary Checking: </div>
                 <div className="ml-4">
-                    <select name="shuffle" onChange={handleSelectDictCheck} value={dictCheck}>
+                    <select
+                        name="shuffle"
+                        onChange={handleSelectDictCheck}
+                        value={dictCheck}
+                    >
                         <option value="1">On</option>
-                        <option value="0">Off</option>   
+                        <option value="0">Off</option>
                     </select>
                 </div>
             </div>
-
         )
     }
 
     const GameType = () => {
-        return(
+        return (
             <div className={styles.horz}>
                 <div>Dictionary Checking: </div>
                 <div className="ml-4">
-                    <select name="shuffle" onChange={handleSelectGameType} value={gameType}>
+                    <select
+                        name="shuffle"
+                        onChange={handleSelectGameType}
+                        value={gameType}
+                    >
                         <option value="75">75 Point Game</option>
-                        <option value="150">150 Point Game</option> 
-                        <option value="10000">Till The Tiles Run Out</option>     
+                        <option value="150">150 Point Game</option>
+                        <option value="10000">Till The Tiles Run Out</option>
                     </select>
                 </div>
             </div>
-
         )
     }
 
 
 
-    // const handleSubmit =(event) =>{
-    //     event.preventDefault()
-    //     let datapack = {
-    //         playerdata: players,
-    //         shuffPlayers: shufflePlayers,
-    //         dictCheck: dictCheck,
-    //         gameType: gameType
-
-    //     }
-    //     setData(datapack)
-    // }
-
-
     return (
         <div className={styles.playerInfo}>
-            <form onSubmit={(e)=>handleSubmit(e, players, shufflePlayers, dictCheck, gameType)}>
+            <form
+                onSubmit={(e) =>
+                    handleSubmit(
+                        e,
+                        players,
+                        shufflePlayers,
+                        dictCheck,
+                        gameType
+                    )
+                }
+            >
                 {playerform}
                 <div className="btn-toolbar">
-                    {players.length<4 ? (
-                    <button type="button" onClick={handleClickHuman}>
-                        Add Human
-                    </button> ) : null}
-                    {players.length<4 ? (
-                    <button type="button" onClick={handleClickAI}>
-                        Add Computer
-                    </button> ) : null}
-                    
-                    {players.length!==0 ? <button type="button" onClick={remove}>
-                        Remove
-                    </button> : null}
+                    {players.length < 4 ? (
+                        <button type="button" onClick={handleClickHuman}>
+                            Add Human
+                        </button>
+                    ) : null}
+                    {players.length < 4 ? (
+                        <button type="button" onClick={handleClickAI}>
+                            Add Computer
+                        </button>
+                    ) : null}
+
+                    {players.length !== 0 ? (
+                        <button type="button" onClick={remove}>
+                            Remove
+                        </button>
+                    ) : null}
                 </div>
-                <ShouldShuffle/>
-                <ShouldCheckDict/>
-                <GameType/>
+                <ShouldShuffle />
+                <ShouldCheckDict />
+                <GameType />
                 <button type="submit">Start Game</button>
             </form>
-
-            
-    
-
-
         </div>
     )
 }
