97a70a23be51338c5bc5516ee458e2d5791a3a94
Sourish Dutta <sourish.d@gmail.com>
2021-08-08 16:15:58 -0500

    handleSubmit defined in FrontPage

--- src/FrontPage/FrontPage.js
+++ src/FrontPage/FrontPage.js
@@ -1,10 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import { Button, Container } from "react-bootstrap"
 import GameInfo from "../GameInfo/GameInfo"
 import styles from "./FrontPage.module.css"
 
 const FrontPage = () => {
-    const gameVariables = []
+    const [gameVariables, setGameVariables] = useState({})
+
+    const handleSubmit = (event, players, shufflePlayers, dictCheck, gameType) =>{
+        event.preventDefault()
+        let x={}
+        x.players=players
+        x.shufflePlayers=shufflePlayers
+        x.dictCheck = dictCheck
+        x.gameType = gameType
+        setGameVariables(x)
+    }
     return (
         <div>
             <div className="p-5 mb-4 bg-light rounded-3">
