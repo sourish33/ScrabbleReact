d47f7503d82755d0f9db3b4da8a656675e31f087
Sourish Dutta <sourish.d@gmail.com>
2021-08-10 00:01:53 -0500

    if no data set, shows Home page if data set shows game page

--- src/FrontPage/FrontPage.js
+++ src/FrontPage/FrontPage.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react"
 import { Container } from "react-bootstrap"
+// import {BrowserRouter as Router, Switch, Route, Link  } from "react-router-dom";
+import Game from "../Game/Game";
+
 import GameInfo from "../GameInfo/GameInfo"
 import styles from './FrontPage.module.css'
+
+
+
+  
+
 
 
 const FrontPage = () => {
@@ -30,17 +38,28 @@ const FrontPage = () => {
              </div>
         )
     }
-    return (
-        <div>
+    const WelcomePage = () => {
+        return (
+            <div>
             <div className={`p-5 mb-4 bg-light rounded-3 ${styles.headerImage}`}>
                 <h1 className={`display-4 `}>Scrabble React!</h1>
                 <h3>A scrabble game built using React.js</h3>
             </div>
-            <Container>
-                <GameInfo handleSubmit={handleSubmit}/>
-            </Container>
-            <ShowData data={gameVariables}/>
-        </div>
+                <Container>
+                    <GameInfo handleSubmit={handleSubmit}/>  
+                    {/* <ShowData data={gameVariables}/>    */}
+                </Container>
+            </div>
+            
+
+        )
+    }
+
+
+    return (
+        <>
+        {Object.keys(gameVariables).length===0 ? <WelcomePage/> : <Game/>}
+        </>
     )
 }
 
