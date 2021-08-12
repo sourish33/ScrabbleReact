7f1ec9abc41952653a0c2205186f65049c3a0234
Sourish Dutta <sourish.d@gmail.com>
2021-08-12 15:47:20 -0500

    keys removed from control buttons

--- src/ControlButtons/ControlButtons.js
+++ src/ControlButtons/ControlButtons.js
@@ -5,30 +5,30 @@ import styles from "./ControlButtons.module.css"
 const ControlButtons = ({shuffleRack, recall, exchange, passTurn, lookup, play, disabled}) => {
     return (
         <div className={`d-flex flex-column ${styles["horz-btns"]}`}>
-            <div key="1" className="p-2 mt-0">
+            <div  className="p-2 mt-0">
                 <Button className={styles.stretch} variant="primary" onClick={shuffleRack} disabled={disabled}>
                 <span className={styles.smallscreen}>Shuffle</span>
                 </Button>
             </div>
-            <div key="2" className="p-2 mt-0">
+            <div  className="p-2 mt-0">
                 <Button className={styles.stretch} variant="primary" onClick={recall} disabled={disabled}>
                 <span className={styles.smallscreen}>Recall</span>
                 </Button>
             </div>
-            <div key="3" className="p-2 mt-0">
+            <div className="p-2 mt-0">
                 <Button className={styles.stretch} variant="primary" onClick={exchange} disabled={disabled}>
                 <span className={styles.smallscreen}>Exchange</span>
                 </Button>
             </div>
-            <div key="4" className="p-2 mt-0">
+            <div className="p-2 mt-0">
                 <Button className={styles.stretch} variant="warning" onClick={passTurn} disabled={disabled}>
                 <span className={styles.smallscreen}>Pass</span>  
                 </Button>
             </div>
-            <div key="5" className="p-2 mt-0">
+            <div className="p-2 mt-0">
                 <Button className={styles.stretch} variant="info" onClick={lookup}><span className={styles.smallscreen}>Dict</span></Button>
             </div>
-            <div key="6" className="p-2 mt-0">
+            <div className="p-2 mt-0">
                 <Button className={styles.stretch} variant="success" onClick={play} disabled={disabled}>
                 <span className={styles.smallscreen}>Play</span>
                 </Button>
