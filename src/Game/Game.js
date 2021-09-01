b3d65d4138aad5e919d3a4d333b74d476580a4e8
Dutta <sourish.dutta@Vanderbilt.Edu>
2021-09-01 10:54:58 -0500

    searcching up to 3-letter words - code syntax cleaned

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -200,12 +200,14 @@ const Game = ({ gameVariables, exitGame }) => {
         let [p1, p2, p3, p4, p5, p6, p7] = makeRackPerms(tiles, playersAndPoints[currentPlayer].rack)
         let makeVerslots = tilesOnBoard(tiles).length !== 0 //no need to make vertical slots if the board is empty
         let [s1, s2, s3, s4, s5, s6, s7] = makeAllSlots(tiles, makeVerslots)
-        let moves = []
-        let moves1 = moveNumber !== 0 ? evaluateMoves(p1, s1, tiles, playersAndPoints[currentPlayer].rack) : []
-        let moves2 = evaluateMoves(p2, s2, tiles, playersAndPoints[currentPlayer].rack)
-        let moves3 = evaluateMoves(p3, s3, tiles, playersAndPoints[currentPlayer].rack)
-        moves = [...moves, ...moves1, ...moves2, ...moves3].sort((x,y)=>y.points-x.points)
-        console.log(moves)
+        let moves = [
+            ...moveNumber !== 0 ? evaluateMoves(p1, s1, tiles, playersAndPoints[currentPlayer].rack) : [], 
+            ...evaluateMoves(p2, s2, tiles, playersAndPoints[currentPlayer].rack), 
+            ...evaluateMoves(p3, s3, tiles, playersAndPoints[currentPlayer].rack)
+        ]
+        moves.sort((x,y)=>y.points-x.points)
+        let toShow = moves.length> 10 ? 10: moves.length
+        console.log(moves.slice(0, toShow))
     }
 
     const lookup = () => {
