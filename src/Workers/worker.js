269a0b701fe95e5de515945c06b66707fa15a1e3
Sourish Dutta <sourish.d@gmail.com>
2021-09-19 03:17:17 -0500

    workers send report of how many words found

--- src/Workers/worker.js
+++ src/Workers/worker.js
@@ -4,6 +4,7 @@ import { evaluateMoves } from "../Game/AIHelperFunctions"
 
 export const crunch = (pp, ss, tiles, whichRack, maxPoints) => {
 
-    postMessage(`working the ${pp[0].length}-letter words`)
-    postMessage(evaluateMoves(pp, ss, tiles, whichRack ) )
+    let result = evaluateMoves(pp, ss, tiles, whichRack )
+    postMessage(`${pp[0].length}-letter words done - ${result.length} found`)
+    postMessage(result)
 }
