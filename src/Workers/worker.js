463523dcb579f16ecc3399823cb7a99704f4c832
Sourish Dutta <sourish.d@gmail.com>
2021-09-25 10:59:22 -0500

    minor

--- src/Workers/worker.js
+++ src/Workers/worker.js
@@ -3,7 +3,6 @@ import { evaluateMoves } from "../Game/AIHelperFunctions"
 
 
 export const crunch = (pp, ss, tiles, whichRack, cutoff, toWin) => {
-
     let result = evaluateMoves(pp, ss, tiles, whichRack, cutoff, toWin )
     postMessage(`Done with ${pp[0].length}-letter words, ${result.length} found `)
     postMessage(result)
