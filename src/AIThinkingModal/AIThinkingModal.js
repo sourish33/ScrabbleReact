a5451716e14cd134cda54fbd908e28d242dab401
Sourish Dutta <sourish.d@gmail.com>
2021-10-03 02:35:06 -0500

    unnecessary imports removed from AIThinking

--- src/AIThinkingModal/AIThinkingModal.js
+++ src/AIThinkingModal/AIThinkingModal.js
@@ -1,24 +1,18 @@
 import Modal from "react-bootstrap/Modal"
-import ModalBody from "react-bootstrap/ModalBody"
-import ModalHeader from "react-bootstrap/ModalHeader"
-import ModalFooter from "react-bootstrap/ModalFooter"
-import ModalTitle from "react-bootstrap/ModalTitle"
 import styles from './AIThinkingModal.module.css'
-import { Button, Spinner } from "react-bootstrap"
+import { Spinner } from "react-bootstrap"
 
 const AIThinkingModal = ({show, aiSays}) => {
 
     let aiUbach = aiSays==="" ? "AI thinking..." : aiSays
     return (
         <Modal show={show}>
-            <ModalBody className={styles.modalbody}>
+            <Modal.Body className={styles.modalbody}>
                 <div className="text-center">
                     <Spinner animation="border" />
                 </div>
                 <div className={styles.spinnertext}>{aiUbach}</div>
-
-
-            </ModalBody>
+            </Modal.Body>
         </Modal>
     )
 }
---
+++ src/Utils/Data.js
@@ -0,0 +1,45 @@
+export const greetings = {
+    normal: [
+    "Nice!",
+    "Finally!",
+    "Good Job!",
+    "INCREDIBLE!",
+    "Noice",
+    "Wowza!",
+    "Woot Woot!",
+    "That's LITERALLY awesome!",
+    "Groovy!",
+    "Righteous!",
+    "Copacetic!",
+    "Nifty!",
+    "Quite a wordsmith there!",
+    "You da best!",
+    "Dat Brain Do",
+    "Brilliant!",
+    "Smart!",
+    "Some Brain Power!",
+    "Unnn-Believable"
+    ], 
+    passExchange: [
+        "Uh Oh!",
+        "Better Luck Next Time!",
+        "It sucks, I know!",
+        "OH MY GOD!",
+        "I'm sorry!",
+        "Not looking good...",
+        "Bad Letters, Huh?"
+    ]
+
+}
+
+export const AI_LIST = [
+    "AI Lily",
+    "AI Tori",
+    "AI Parker",
+    "AI Kira",
+    "AI Jasmine",
+    "AI Kashmir",
+    "AI Dylan",
+    "AI Sienna",
+    "AI Sassy",
+]
