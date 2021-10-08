b961211c237bcda295af834bd38d1c02e57cbe0f
Sourish Dutta <sourish.d@gmail.com>
2021-10-08 01:39:35 -0500

    working

--- src/BoardAndRack.js
+++ src/BoardAndRack.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from "react"
+import React, { useEffect }  from "react"
 import Board from "./Board/Board"
 import styles from "./BoardAndRack.module.css"
 import {emptyOnRack} from "./Game/GameHelperFunctions"
