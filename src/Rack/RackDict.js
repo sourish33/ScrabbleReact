ae33f76382c4c050dc860ea6a0c5ecf0432b7c6e
SD <sourish.d@gmail.com>
2022-02-03 16:34:29 -0600

    Rack shown in Dict modal shows all tiles, ehether on board or rack

--- src/Rack/RackDict.js
+++ src/Rack/RackDict.js
@@ -2,12 +2,19 @@ import React from "react";
 import styles from "./RackEx.module.css";
 import RackSquareEx from "../RackSquare/RackSquareEx";
 import TileDict from "../Tile/TileDict";
+import { tilesOnRack, tilesPlayedNotSubmitted } from "../Game/GameHelperFunctions";
 
 const RackDict = ({ whichRack, tiles }) => {
 
+  const tr = tilesOnRack(tiles, whichRack)
+  const tpns = tilesPlayedNotSubmitted(tiles)
+  const currentUnPlayedTiles = [...tr, ...tpns]
+  console.log(currentUnPlayedTiles)
+
   const squares = [];
   for (let i = 1; i < 8; i++) {
-      let tile = tiles.filter((el)=>{return el.pos === whichRack+i})[0]
+      // let tile = tiles.filter((el)=>{return el.pos === whichRack+i})[0]
+      let tile = currentUnPlayedTiles[i-1]
     let piece = (
       <TileDict
         letter={tile ? tile.letter : null} //just in case tile is undefined
