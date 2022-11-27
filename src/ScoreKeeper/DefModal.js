52fff1d00ed35167d3bc24ae64cb1c166b138fe5
SD <sourish.d@gmail.com>
2022-11-27 17:00:54 -0600

    Displaying definitions in modal

--- src/ScoreKeeper/DefModal.js
+++ src/ScoreKeeper/DefModal.js
@@ -1,30 +1,45 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
+import { Spinner } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import styles from './DefModal.module.css'
 
 export const DefModal = ({show, setShow, word}) => {
 //   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [showSpinner, setShowSpinner] = useState(true)
+  const [definition, setDefinition] = useState("")
+
+  useEffect(()=>{
+    fetch(`https://helpful-deer-baseball-cap.cyclic.app/dictionary?word=${word}`)
+    .then(res=>res.json())
+    .then((data)=>{
+        setShowSpinner(false)
+        if (data.definition.length>0){
+            console.log(data.definition[0].Definition)
+            setDefinition(data.definition[0].Definition)
+        } else{
+            setDefinition("No definition found")
+        }
+        
+    })
+    
+
+  },[word])
 
   return (
     <>
-      {/* <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button> */}
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>{word}</Modal.Title>
         </Modal.Header>
-        <Modal.Body>{}</Modal.Body>
+        <Modal.Body><Spinner animation="grow" variant="info" className={showSpinner?``:`${styles.hidden}`}/>{definition}</Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
